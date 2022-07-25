f356ddbe40dbf2138849629606d7d0660ba8397c
Sean Wong <seanwong24@live.com>
2022-07-25 09:01:21 +0000

    add "checkbox" part for checkbox component

--- src/components/awesome-checkbox/awesome-checkbox.tsx
+++ src/components/awesome-checkbox/awesome-checkbox.tsx
@@ -28,17 +28,20 @@ export class AwesomeCheckbox implements ComponentInterface {
       <Host>
         <input id="native" type="checkbox" checked={this.checked} disabled={this.disabled} hidden onChange={this.handleCheckboxChange} />
         <label part="container" htmlFor="native">
-          {this.renderCheckMark()}
+          {this.renderCheckbox()}
+          <slot></slot>
         </label>
       </Host>
     );
   }
 
-  private renderCheckMark() {
+  private renderCheckbox() {
     return (
-      <svg class="check-mark" viewBox="0 0 100 100">
-        <path d="M10,55L40,80L90,20" fill="none" stroke="var(--awesome-ui-color-contrast)" stroke-width={20} />
-      </svg>
+      <div part="checkbox">
+        <svg class="check-mark" viewBox="0 0 100 100">
+          <path d="M10,55L40,80L90,20" fill="none" stroke="var(--awesome-ui-color-contrast)" stroke-width={20} />
+        </svg>
+      </div>
     );
   }
 
--- src/components/awesome-checkbox/readme.md
+++ src/components/awesome-checkbox/readme.md
@@ -24,6 +24,7 @@
 
 | Part          | Description |
 | ------------- | ----------- |
+| `"checkbox"`  |             |
 | `"container"` |             |
 
 
